ed1e3230b2d829452f9664fb0d2ea7743d76070e
Darklaw-game <darklaw.game@gmail.com>
2025-04-23 18:37:59 -0500

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -164,7 +164,7 @@ end
 
 
 --[[ Version ]]--
-local curVer = isAprilFools() and Format("%d.%d.%d", math.random(1, 10), math.random(0, 99), math.random(0, 99)) or "2.4.1"
+local curVer = isAprilFools() and Format("%d.%d.%d", math.random(1, 10), math.random(0, 99), math.random(0, 99)) or "1.0.0"
 
 --[[ Brand ]]--
 local mainName = 'Nameless Admin'
